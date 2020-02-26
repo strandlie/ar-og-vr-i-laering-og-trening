18efeb617eeda1f0153cd933310746c0cbc7cd11
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-02-26 11:56:46 +0100

    Add some fire prefabs

---
+++ Assets/fjompo_grande/scripts/render_only_in_inspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class render_only_in_inspector : MonoBehaviour
+{
+    public MeshRenderer target;
+
+    void Start()
+    {
+        if (this.enabled) {
+            target.enabled = false;
+        }
+    }
+
+    void OnEnable()
+    {
+        target.enabled = false;
+    }
+    
+    void OnDisable()
+    {
+        target.enabled = true;
+    }
+}
