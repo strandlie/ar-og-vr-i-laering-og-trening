a1e07283ead420e11a444cba5055b139d38896e2
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-02-26 14:20:53 +0100

    Make the collision trigger between foam and fire core work. Destroy foam on trigger enter

---
+++ Assets/fjompo_grande/scripts/burnable_behaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burnable_behaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("setup " + this.gameObject.name);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // when hit by foam
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("enter " + other.transform.parent.gameObject.name);
+        if (other.transform.parent.gameObject.name.StartsWith("foam_particle"))
+        {
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+
+
+}
