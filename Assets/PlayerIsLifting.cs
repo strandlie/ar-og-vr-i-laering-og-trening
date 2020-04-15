96990431c756fd0618329a063d8daf529436598e
Håkon Strandlie <haakstr@stud.ntnu.no>
2020-04-15 12:26:51 +0200

    Added important settings files in Unity

---
+++ Assets/PlayerIsLifting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using static OVRHand;
+
+public class PlayerIsLifting : MonoBehaviour
+{
+    //OVRHand[] hands;
+    bool isIndexFingerPinching = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       //hands = GetComponents<OVRHand>();
+     
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("");
+        //Debug.Log("HandLogInstance: " + hands[0].gameObject.tag);
+        //Debug.Log("Is pinching: " + hands[0].GetFingerIsPinching(HandFinger.Index));
+        /*Debug.Log("HandLogInstance: " + hands[1].tag);
+        Debug.Log("Is pinching: " + hands[1].GetFingerIsPinching(HandFinger.Index));
+        Debug.Log("####### ");
+
+        if (isIndexFingerPinching) {
+            Debug.Log("IndexFinger is pinching");
+        }
+        */
+       
+    }
+}
