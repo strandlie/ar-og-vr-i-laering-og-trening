3f8a5addcd558d7b41eb7b06c2654984a932184a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-04-15 13:43:02 +0200

    Hei bjarne

--- Assets/fjompo_grande/scripts/GrabableComponentEnabler.cs
+++ Assets/fjompo_grande/scripts/GrabableComponentEnabler.cs
@@ -20,12 +20,16 @@ public class GrabableComponentEnabler : MonoBehaviour
         if (componentToEnable != null && grabbable.isGrabbed) {
             // TODO: determine if left or right controller
             // grabbable.isGrabbed.m_controller is what i'm looking for, but it's private
-            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) > 0.7f || 
-                OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch) > 0.7f) {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) > 0.7f ||
+                OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) > 0.7f) {
                     if (!componentToEnable.enabled) { componentToEnable.enabled = true; }
-            } else {
-                    if (componentToEnable.enabled) { componentToEnable.enabled = false; }
             }
+            else {
+                if (componentToEnable.enabled) { componentToEnable.enabled = false; }
+            }
+        }
+        else {
+            if (componentToEnable.enabled) { componentToEnable.enabled = false; }
         }
     }
 }
